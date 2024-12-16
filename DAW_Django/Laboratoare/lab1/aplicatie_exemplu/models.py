9998b2df0bbec720fc90362f80cadf57ce7c8feb
Daria <82290545+dariadragomir@users.noreply.github.com>
2024-12-16 15:45:18 +0200

    Update models.py

--- DAW_Django/Laboratoare/lab1/aplicatie_exemplu/models.py
+++ DAW_Django/Laboratoare/lab1/aplicatie_exemplu/models.py
@@ -1,28 +1,81 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
-from django.db import models
-import uuid
+import sys
+sys.path.append('/Users/dariadragomir/Facultate/AN2/SEM1/Django/Lab1/')
 
-class Organizator(models.Model):
-    nume = models.CharField(max_length=100)
-    email = models.EmailField()
+class Festival(models.Model):
+    cod_festival = models.CharField(max_length=50)
+    denumire = models.CharField(max_length=100)
+    eveniment_muzical = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.denumire
 
 class Locatie(models.Model):
-    adresa = models.CharField(max_length=255)
-    oras = models.CharField(max_length=100)
-    judet = models.CharField(max_length=100)
-    cod_postal = models.CharField(max_length=10)
+    cod_locatie = models.CharField(max_length=50)
+    oras = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.oras
 
-class Prajitura(models.Model):
-    nume = models.CharField(max_length=50, unique=True)
-    descriere = models.TextField(blank=True, null=True)
-    pret = models.DecimalField(max_digits=8, decimal_places=2)
-    gramaj = models.IntegerField()
-    tip_produs = models.CharField(max_length=50, default='cofetarie')
-    calorii = models.IntegerField()
-    categorie = models.CharField(max_length=50, default='comuna')
-    pt_diabetici = models.BooleanField(default=False)
-    imagine = models.CharField(max_length=300, blank=True, null=True)
-    data_adaugare = models.DateTimeField(auto_now_add=True)
+class Partener(models.Model):
+    cod_partener = models.CharField(max_length=50)
+    nume = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nume
+class Concert(models.Model):
+    partener = models.ForeignKey(Partener, on_delete=models.CASCADE)
+    locatie = models.ForeignKey(Locatie, on_delete=models.CASCADE)
+    festival = models.ForeignKey(Festival, on_delete=models.CASCADE)
+    cod_concert = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.cod_concert
+
+class Scena(models.Model):
+    concert = models.ForeignKey(Concert, on_delete=models.CASCADE)
+    nume = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nume
+
+class TipBilet(models.Model):
+    cod_bilet = models.CharField(max_length=50)
+    tip = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.tip
+
+class Bilet(models.Model):
+    cod_bilet = models.ForeignKey(TipBilet, on_delete=models.CASCADE)
+    concert = models.ForeignKey(Concert, on_delete=models.CASCADE)
+
+class Client(models.Model):
+    cod_bilet = models.ForeignKey(Bilet, on_delete=models.CASCADE)
+    nume = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nume
+
+class Repertoriu(models.Model):
+    concert = models.ForeignKey(Concert, on_delete=models.CASCADE)
+
+class Piesa(models.Model):
+    cod_repertoriu = models.ForeignKey(Repertoriu, on_delete=models.CASCADE)
+    nume = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nume
+
+class Artist(models.Model):
+    cod_piesa = models.ForeignKey(Piesa, on_delete=models.CASCADE)
+    nume = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nume
+
+class CustomUser(AbstractUser):
+    telefon = models.CharField(max_length=15, blank=True)
+
